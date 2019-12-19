5f095ad0d8ba609b0378c4a2a6b198c876f16f7a
Agnes <agnesft5@gmail.com>
2019-12-19 10:12:59 +0100

    Static server & ng build done

---
+++ STATIC-SERVER/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const https = require('https');
+
+const fs = require('fs');
+const server = express();
+
+
+
+server.use(express.static('public'));
+
+https.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/live/weeklydiet.es/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/weeklydiet.es/fullchain.pem')
+}, server).listen(443, ()=>{
+    console.log('Servidor escuchando en el puerto 443');
+})
